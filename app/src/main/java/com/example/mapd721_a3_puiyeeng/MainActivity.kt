f050b075f57f92f6f08f72d97827c29ed0d995ea
Pui Yee Ng <hilarypyng@gmail.com>
2024-03-20 11:14:26 -0400

    Implement Enter Exit Animation, home screen and navigation

--- app/src/main/java/com/example/mapd721_a3_puiyeeng/MainActivity.kt
+++ app/src/main/java/com/example/mapd721_a3_puiyeeng/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mapd721_a3_puiyeeng
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloat
@@ -11,6 +12,9 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,10 +23,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -32,12 +43,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.mapd721_a3_puiyeeng.ui.theme.MAPD721A3PuiYeeNgTheme
 import kotlinx.coroutines.delay
+import androidx.navigation.compose.composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +66,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RocketAnimationScreen()
+                    AnimationApp()
                 }
             }
         }
@@ -57,37 +74,116 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun RocketAnimationScreen() {
-    var isRocketLaunched by remember { mutableStateOf(false) }
+fun AnimationApp() {
+    val navController = rememberNavController()
 
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .offset(
-                    y = animateFloatAsState(
-                        targetValue = if (isRocketLaunched) 100f else 300f,
-                        animationSpec = tween(durationMillis = 1000), label = ""
-                    ).value.dp
-                )
-        ) {
-            Button(
-                onClick = {
-                    isRocketLaunched = !isRocketLaunched
-                }
-            ) {
-                Text(if (isRocketLaunched) "Land Rocket" else "Launch Rocket")
-            }
+    NavHost(navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(navController)
         }
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.TopEnd
-        ) {
-            RocketImage(isRocketLaunched)
+        composable("rocket") {
+            RocketAnimationScreen(navController)
+        }
+        composable("scale") {
+            ScaleAnimationScreen(navController)
+        }
+        composable("infinite") {
+            InfiniteAnimationScreen(navController)
+        }
+        composable("enterExit") {
+            ExitAnimationScreen(navController)
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavController) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = { navController.navigate("rocket") },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = "Transition Animation")
+                }
+                Button(
+                    onClick = { navController.navigate("scale") },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = "Scale Animation")
+                }
+                Button(
+                    onClick = { navController.navigate("infinite") },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = "Infinite Animation")
+                }
+                Button(
+                    onClick = { navController.navigate("enterExit") },
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = "Enter Exit Animation")
+                }
+            }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RocketAnimationScreen(navController: NavController) {
+    var isRocketLaunched by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Animation Samples") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.popBackStack() },
+                    ) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        },
+        content = { padding ->
+            Box(
+                modifier = Modifier.fillMaxSize().padding(padding)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .offset(
+                            y = animateFloatAsState(
+                                targetValue = if (isRocketLaunched) 100f else 300f,
+                                animationSpec = tween(durationMillis = 1000), label = ""
+                            ).value.dp
+                        )
+                ) {
+                    Button(
+                        onClick = {
+                            isRocketLaunched = !isRocketLaunched
+                        }
+                    ) {
+                        Text(if (isRocketLaunched) "Land Rocket" else "Launch Rocket")
+                    }
+                }
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.TopEnd
+                ) {
+                    RocketImage(isRocketLaunched)
+                }
+            }
+        }
+    )
 }
 
 @Composable
@@ -102,7 +198,7 @@ fun RocketImage(isRocketLaunched: Boolean) {
             }
         }, label = "rocketLocation"
     ) { launched ->
-        if (launched) 0f else 550f
+        if (launched) 0f else 500f
     }
     val scale by transition.animateFloat(
         transitionSpec = {
@@ -124,9 +220,9 @@ fun RocketImage(isRocketLaunched: Boolean) {
 
 
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScaleAnimation() {
+fun ScaleAnimationScreen(navController: NavController) {
     val buttonIsClicked = remember { mutableStateOf(false) }
     val scaleState = remember { mutableStateOf(1f) }
     val clickTime = remember { mutableStateOf(0) }
@@ -149,72 +245,166 @@ fun ScaleAnimation() {
         animationSpec = spring()
     ).value
 
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Button(
-            onClick = {
-                buttonIsClicked.value = !buttonIsClicked.value
-                if (buttonIsClicked.value){
-                    if (clickTime.value < 3){
-                        clickTime.value += 1
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Animation Samples") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.popBackStack() },
+                    ) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
                     }
-                    else{
-                        clickTime.value = 1
+                }
+            )
+        },
+        content = { padding ->
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize().padding(padding)
+            ) {
+                Button(
+                    onClick = {
+                        buttonIsClicked.value = !buttonIsClicked.value
+                        if (buttonIsClicked.value) {
+                            if (clickTime.value < 3) {
+                                clickTime.value += 1
+                            } else {
+                                clickTime.value = 1
+                            }
+                        }
+
+                    },
+                    modifier = Modifier
+                        .size(200.dp, 80.dp)
+                        .scale(scaleState.value),
+                    shape = RectangleShape
+                ) {
+                    Text("Click Me")
+                }
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InfiniteAnimationScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Animation Samples") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.popBackStack() },
+                    ) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        },
+        content = { padding ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(padding),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                var isToggled by remember { mutableStateOf(false) }
+
+                val scale by animateFloatAsState(
+                    targetValue = if (isToggled) 1.8f else 1f,
+                    animationSpec = infiniteRepeatable(
+                        animation = tween(durationMillis = 1000),
+                        repeatMode = RepeatMode.Reverse
+                    )
+                )
+
+                LaunchedEffect(Unit) {
+                    while (true) {
+                        isToggled = !isToggled
+                        delay(1000)
                     }
                 }
 
-                      },
-            modifier = Modifier
-                .size(200.dp, 80.dp)
-                .scale(scaleState.value),
-            shape = RectangleShape
-        ) {
-            Text("Click Me")
-        }
-    }
-}
-
-@Composable
-fun InfiniteAnimation() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        var isToggled by remember { mutableStateOf(false) }
-
-        val scale by animateFloatAsState(
-            targetValue = if (isToggled) 1.8f else 1f,
-            animationSpec = infiniteRepeatable(
-                animation = tween(durationMillis = 1000),
-                repeatMode = RepeatMode.Reverse
-            )
-        )
-
-        LaunchedEffect(Unit) {
-            while (true) {
-                isToggled = !isToggled
-                delay(1000)
+                Image(
+                    painter = painterResource(R.drawable.stuart),
+                    contentDescription = "Stuart",
+                    modifier = Modifier
+                        .size(200.dp)
+                        .scale(scale)
+                )
             }
         }
-
-        Image(
-            painter = painterResource(R.drawable.stuart),
-            contentDescription = "Toggle Scale Image",
-            modifier = Modifier
-                .size(200.dp)
-                .scale(scale)
-        )
-    }
+    )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExitAnimationScreen(navController: NavController) {
+    var visible by remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Animation Samples") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.popBackStack() },
+                    ) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        },
+        content = { padding ->
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    AnimatedVisibility(
+                        visible = visible,
+                        enter = slideInHorizontally(
+                            initialOffsetX = { -it },
+                            animationSpec = tween(durationMillis = 1000) // Adjust the duration here
+                        ),
+                        exit = slideOutVertically()
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .padding(top = 32.dp)
+                                .size(256.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.cat),
+                                contentDescription = "Cat",
+                                modifier = Modifier.fillMaxSize()
+                            )
+                            Canvas(modifier = Modifier.fillMaxSize()) {
+                                val radius = size.minDimension / 2
+                                drawCircle(
+                                    color = Color.Gray,
+                                    radius = radius,
+                                    style = Stroke(width = 4.dp.toPx())
+                                )
+                            }
+                        }
+                    }
+                    Button(onClick = { visible = !visible }) {
+                        Text(text = if (visible) "Press for Exit Animation" else "Press for Enter Animation")
+                    }
+                }
+            }
+        }
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
-fun RocketAnimationScreenPreview() {
+fun ExitAnimationScreenPreview() {
     MAPD721A3PuiYeeNgTheme {
-        RocketAnimationScreen()
+        AnimationApp()
     }
 }
