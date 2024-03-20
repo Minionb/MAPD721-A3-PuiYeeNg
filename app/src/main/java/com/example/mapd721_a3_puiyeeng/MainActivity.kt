0dc1a96117fc0df275a9161da63c8ffcd81c7f3a
Pui Yee Ng <hilarypyng@gmail.com>
2024-03-19 20:59:48 -0400

    Implement transition animation

--- app/src/main/java/com/example/mapd721_a3_puiyeeng/MainActivity.kt
+++ app/src/main/java/com/example/mapd721_a3_puiyeeng/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.example.mapd721_a3_puiyeeng
 
 import android.os.Bundle
-import android.view.animation.ScaleAnimation
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,7 +31,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
@@ -52,12 +49,81 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InfiniteAnimation()
+                    RocketAnimationScreen()
                 }
             }
         }
     }
 }
+
+@Composable
+fun RocketAnimationScreen() {
+    var isRocketLaunched by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .offset(
+                    y = animateFloatAsState(
+                        targetValue = if (isRocketLaunched) 100f else 300f,
+                        animationSpec = tween(durationMillis = 1000), label = ""
+                    ).value.dp
+                )
+        ) {
+            Button(
+                onClick = {
+                    isRocketLaunched = !isRocketLaunched
+                }
+            ) {
+                Text(if (isRocketLaunched) "Land Rocket" else "Launch Rocket")
+            }
+        }
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopEnd
+        ) {
+            RocketImage(isRocketLaunched)
+        }
+    }
+}
+
+@Composable
+fun RocketImage(isRocketLaunched: Boolean) {
+    val transition = updateTransition(targetState = isRocketLaunched, label = "rocketTransition")
+    val translateY by transition.animateFloat(
+        transitionSpec = {
+            if (targetState) {
+                tween(durationMillis = 1000)
+            } else {
+                spring(stiffness = Spring.StiffnessLow)
+            }
+        }, label = "rocketLocation"
+    ) { launched ->
+        if (launched) 0f else 550f
+    }
+    val scale by transition.animateFloat(
+        transitionSpec = {
+            tween(durationMillis = 1000)
+        }, label = "rocketScale"
+    ) { launched ->
+        if (launched) 0.5f else 1f
+    }
+
+
+    Image(
+        painter = painterResource(R.drawable.rocket),
+        contentDescription = "Rocket",
+        modifier = Modifier
+            .size((200 * scale).dp)
+            .offset(y = translateY.dp)
+    )
+}
+
+
+
 
 @Composable
 fun ScaleAnimation() {
@@ -147,8 +213,8 @@ fun InfiniteAnimation() {
 
 @Preview(showBackground = true)
 @Composable
-fun ScaleAnimationPreview() {
+fun RocketAnimationScreenPreview() {
     MAPD721A3PuiYeeNgTheme {
-        InfiniteAnimation()
+        RocketAnimationScreen()
     }
 }
